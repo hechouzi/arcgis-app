01c2be079c002b2dde1829e1924e410d4ae5ec0e
hechengwei <hechengwei@piesat.cn>
2020-03-20 17:50:39 +0800

    commit all

---
+++ src/component/1_arcgis_container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ArcgisContainer extends Component {
+  render() {
+    return (
+      <div>
+        arcgisDemo
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducers from './redux/reducers';
+import IndexRoute from './router/index-router';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <IndexRoute></IndexRoute>
+    </HashRouter>
+  </Provider>,
+  document.getElementById('root'));
+serviceWorker.unregister();
